cb64971ee4f243b0ab1123aa2b3ba51ed2dfcbb4
justdorm <justin.dormitzer@gmail.com>
2020-05-06 19:56:51 -0400

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ let index = 0
 let images = document.getElementById("images")
 let image = document.getElementById("image")
 function ImageCycle(){
-  images.requestFullscreen()
+  openFullscreen()
   image.src = imageLibrary[index]
   index += 1
   if (index == imageLibrary.length) {
@@ -18,5 +18,16 @@ function ImageCycle(){
   }
 }
 
+function openFullscreen() {
+  if (images.requestFullscreen) {
+    images.requestFullscreen();
+  } else if (images.mozRequestFullScreen) { /* Firefox */
+    images.mozRequestFullScreen();
+  } else if (images.webkitRequestFullscreen) { /* Chrome, Safari & Opera */
+    images.webkitRequestFullscreen();
+  } else if (images.msRequestFullscreen) { /* IE/Edge */
+    images.msRequestFullscreen();
+  }
+}
 
 images.addEventListener("click", ImageCycle)
